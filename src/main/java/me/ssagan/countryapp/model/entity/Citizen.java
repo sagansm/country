4a844149e15df90ef21ae2f6188165d96257a0f3
Светлана <aminova.svekta@gmail,com>
2023-07-15 22:33:25 +0300

    add Country and Citizen

---
+++ src/main/java/me/ssagan/countryapp/model/entity/Citizen.java
@@ -0,0 +1,40 @@
+package me.ssagan.countryapp.model.entity;
+
+public class Citizen {
+    private int id;
+    private String surname;
+    private String name;
+    private int age;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
---
+++ src/main/java/me/ssagan/countryapp/model/entity/Country.java
@@ -0,0 +1,22 @@
+package me.ssagan.countryapp.model.entity;
+
+public class Country {
+    private String name;
+    private String capital;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCapital() {
+        return capital;
+    }
+
+    public void setCapital(String capital) {
+        this.capital = capital;
+    }
+}

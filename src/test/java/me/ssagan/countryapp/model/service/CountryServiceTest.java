7c56cb531a07819fae51852c104d0934a5dc37b7
Светлана <aminova.svekta@gmail,com>
2023-07-21 00:36:50 +0300

    fix Tests

--- src/test/java/me/ssagan/countryapp/model/service/CountryServiceTest.java
+++ src/test/java/me/ssagan/countryapp/model/service/CountryServiceTest.java
@@ -1,37 +1,50 @@
 package me.ssagan.countryapp.model.service;
 
-import me.ssagan.countryapp.model.entity.*;
+import me.ssagan.countryapp.model.entity.Citizen;
+import me.ssagan.countryapp.model.entity.Country;
+import me.ssagan.countryapp.model.entity.Region;
 import org.testng.Assert;
-import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 public class CountryServiceTest {
     private CountryService service;
     private Country country;
 
-    @BeforeMethod
-    private void setUp(){
+    @BeforeTest
+    private void setUp() {
         service = CountryService.getInstance();
         country = Country.getInstance();
     }
 
-    @Test(description = "подсчет среднего возраста граждан")
-    public void getCitizenAverageAge() {
+    @DataProvider(name = "citizensForAverageAge")
+    public Object[][] createData() {
         country.addCitizen(new Citizen("Фамилия1", "Имя1", 18, null), null);
         country.addCitizen(new Citizen("Фамилия2", "Имя2", 19, null), null);
         country.addCitizen(new Citizen("Фамилия3", "Имя3", 20, null), null);
         country.addCitizen(new Citizen("Фамилия4", "Имя4", 21, null), null);
         country.addCitizen(new Citizen("Фамилия5", "Имя5", 22, null), null);
-        int actual = service.getCitizenAverageAge(country);
-        Assert.assertEquals(actual, 20);
+        return new Object[][]{{country, 20}};
     }
 
-    @Test()
-    public void countRegion(){
+    @DataProvider(name = "regionsForCount")
+    public Object[][] createReg() {
         country.addRegion(new Region("Ярославская область", country));
         country.addRegion(new Region("Московская область", country));
         country.addRegion(new Region("Костромская область", country));
-        int actual =  service.countRegion(country);
-        Assert.assertEquals(actual, 3);
+        return new Object[][]{{country, 3}};
+    }
+
+    @Test(dataProvider = "citizensForAverageAge", description = "подсчет среднего возраста граждан")
+    public void getCitizenAverageAge(Country country, int expected) {
+        int actual = service.getCitizenAverageAge(country);
+        Assert.assertEquals(actual, expected);
+    }
+
+    @Test(dataProvider = "regionsForCount", description = "количство областей")
+    public void countRegion(Country country, int expected) {
+        int actual = service.countRegion(country);
+        Assert.assertEquals(actual, expected);
     }
 }
